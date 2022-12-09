0ecb6f27241a58d57c592f6405fba9188124bf94
senatorfirst1 <senatorfirst1@gmail.com>
2022-12-09 16:00:16 +0100

    JavaScript 0.0.0.11 - 11_Primitives

---
+++ 11_Primitives.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+const string1 = new String("Hello, World"); // It is an object
+
+const string2 = "Hello, World"; // It is a string primitive
+const number = 234; // It is a number primitive
+const hugeNumber = BigInt(43753459593757); // It is a big integer (number) primitive
+const myBo = true; // It is a boolean primitive
+const mySym = Symbol("hi"); // It is a symbol primitive
+
+console.log(string1 === string2);
+console.log(string1 == string2);
+
+
+
+
+
+
+
+// node 11_Primitives.js // FOR RUN
