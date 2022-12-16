fe33da23b95769d8b76fa3b887e46778181a0106
senatorfirst1 <senatorfirst1@gmail.com>
2022-12-16 18:15:28 +0100

    JavaScript 0.0.0.11 - 11_Primitives

--- 10_CallBack_Functions.js
+++ 10_CallBack_Functions.js
@@ -26,8 +26,8 @@ function mainFunction(randomNumber, shouldCall, callBack) {
 
 
 console.log(mainFunction(20, true, myCallBack1));
-console.log(mainFunction(20, true, myCallBack2));
-console.log(mainFunction(20, true, myCallBack3));
+// console.log(mainFunction(20, true, myCallBack2));
+// console.log(mainFunction(20, true, myCallBack3));
 
 
 
